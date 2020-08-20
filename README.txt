3effe7ff81922d12eeb63bcefdca05acd8992ba8
robinstgeorges <robin.sgeorges@gmail.com>
2020-08-20 15:21:20 +0200

    added cours routes

--- README.txt
+++ README.txt
@@ -1,2 +1,28 @@
 py -m venv env
 source env/Scripts/activate
+
+$ export FLASK_APP=app.py
+$ python -m flask run
+
+
+|||||||||||||||||
+$ py -m venv flask
+
+$ cd flask
+
+Now let's activate the virtualenv:
+
+$ cd flask/scripts
+
+$ activate
+
+Now you should see (flask) on the left of the command line.
+
+Let's install flask:
+
+$ pip install flask
+
+$ pip install mysql-python
+$ pip install mysqlclient
+python -m pip install pyyaml
+pip install flask-mysqldb
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, redirect, jsonify
 from flask_mysqldb import MySQL
-import yaml 
+import yaml
+import logging
 
 app = Flask(__name__)
 
-#	Config
+#   Config
 db = yaml.load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
@@ -13,22 +14,98 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
+
 @app.route('/')
 def index():
-	return '<h1>bienvenue sur l\'api de Tesserakt python flask</h1>';
-	
-#	cours
-@app.route('/getAllCours')
+    return '<h1>bienvenue sur l\'api de Tesserakt python flask</h1>';
+
+
+##################################################
+#   cours
+##################################################
+
+
+@app.route('/cours')
 def getAllCours():
-	cur = mysql.connection.cursor()
-	coursCur = cur.execute('select * from cours')
-	if coursCur > 0:
-		lesCours = cur.fetchall()
-		return jsonify(lesCours)
-		#return render_template('cours.html',cours = lesCours)
+    cur = mysql.connection.cursor()
+    coursCur = cur.execute('select * from cours')
+    if coursCur > 0:
+        lesCours = cur.fetchall()
+        cur.close()
+        return jsonify(lesCours)
+    cur.close()
 
 
-#	Exercice
-	
+@app.route('/getCours/<id>')
+def getCoursById(id):
+    cur = mysql.connection.cursor()
+    coursCur = cur.execute('select * from cours where id_cours = ' + id)
+    if coursCur > 0:
+        coursById = cur.fetchall()
+        cur.close()
+        return jsonify(coursById)
+    cur.close()
+
+
+##################################################
+#   Exercice
+##################################################
+
+@app.route('/exercices')
+def getAllExercices():
+    cur = mysql.connection.cursor()
+    exercicesCur = cur.execute('select * from exercices')
+    if exercicesCur > 0:
+        exercices = cur.fetchall()
+        cur.close()
+        return jsonify(exercices)
+    cur.close()
+
+
+@app.route('/getExercice/<id>')
+def getExerciceById(id):
+    cur = mysql.connection.cursor()
+    exerciceCur = cur.execute('select * from exercices where id_exercice = ' + id)
+    if exerciceCur > 0:
+        exercice = cur.fetchall()
+        cur.close()
+        return jsonify(exercice)
+    cur.close()
+
+
+@app.route('/setIsStarted/<id>/<email>')
+def setIsStartedById(id, email):
+    mysql.connection.autocommit(on=True)
+    cur = mysql.connection.cursor()
+    email = email.replace('%40', '@')
+    email = email.replace('%point', '.')
+    curResult = cur.execute('update userdata set is_started = true, date_start = CURRENT_TIMESTAMP where id_exercice = ' + id + ' and email like "' + email + '"')
+    if curResult > 0:
+        result = cur.fetchall()
+        cur.close()
+        return jsonify(1)
+    cur.close()
+    app.logger.info(email)
+    return jsonify(0)
+
+@app.route('/setIsFinished/<id>/<email>')
+def setIsFinishedById(id, email):
+    mysql.connection.autocommit(on=True)
+    cur = mysql.connection.cursor()
+    email = email.replace('%40', '@')
+    email = email.replace('%point', '.')
+    curResult = cur.execute('update userdata set is_finished = true, date_start = CURRENT_TIMESTAMP where id_exercice = ' + id + ' and email like "' + email + '"')
+    if curResult > 0:
+        cur.close()
+        return jsonify(1)
+    cur.close()
+    app.logger.info(email)
+    return jsonify(0)
+
+
+##################################################
+#   Reponse
+##################################################
+
 if __name__ == '__main__':
-      app.run(debug = true, host='0.0.0.0', port=8081)
+    app.run(debug=True)
